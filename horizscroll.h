52141797a912379e18df2e93001e190b5d2328b3
AnthonyMoreno01 <55465354+AnthonyMoreno01@users.noreply.github.com>
2021-10-28 23:05:12 -0500

    finished function comments

--- horizscroll.h
+++ horizscroll.h
@@ -1,27 +1,23 @@
 typedef struct {
-	byte x; // low byte is sub-pixel
-	byte y;
+	byte x; 	// hero x coordinate
+	byte y; 	// hero y coordinate
+  	byte dir; 	// hero direction of movement
+  	int collided:1; // hero collided
+        word bit1; 	// hero score    1-9
+  	word bit2; 	// hero score   10-99
+  	word bit3; 	// hero score  100-999
+  	word bit4; 	// hero score 1000-9990
 
-  	byte dir;
-  	int collided:1;
-        word bit1;
-  	word bit2;
-  	word bit3;
-  	word bit4;
-	
-  	 char asset1;
-  	 char asset2;
-  	 char asset3;
-  	 char asset4;
 } Hero;
 
 typedef struct {
-	byte x; // low byte is sub-pixel
-	byte y;
+	byte x; 	// heart x coordinate
+	byte y;		// heart y coordinate
 
 } Heart;
 
 
+//prototypes
 void play(void);
 void init_game(void);
 void game_over(void);
